c7ae2b5b173e7b15d7c2e242e45268c096dc6d57
Gaurav <gauravgargcs@gmail.com>
2021-10-29 17:33:08 +0530

    chat diffrent port still issue

--- routes/index.js
+++ routes/index.js
@@ -11,20 +11,16 @@ res.send("Hello World");
 
 
 /* GET home page. */
-router.get('/name', function(req, res, next) {
-	
+router.get('/name', function(req, res, next) {	
   res.render('index', { title: 'hello' });
-  
 });
+
 
 
 /* GET home page. */
-router.get('/test', function(req, res, next) {
-	
+router.get('/test', function(req, res, next) {	
   res.render('index', { title: 'test' });
-  
 });
-
 
 
 module.exports = router;
